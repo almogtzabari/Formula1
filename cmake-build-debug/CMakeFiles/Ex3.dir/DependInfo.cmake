544228d0b98df8f828a663e04b147bdf03418fd9
almogtzabari <almogtzabari@campus.technion.ac.il>
2018-04-28 20:00:30 +0300

    uploaded files

---
+++ cmake-build-debug/CMakeFiles/Ex3.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/Almog/OneDrive - Technion/Simester 3/234122 - Introduction to Systems Programming/Homework/HW2/Formula1/driver.c" "C:/Users/Almog/OneDrive - Technion/Simester 3/234122 - Introduction to Systems Programming/Homework/HW2/Formula1/cmake-build-debug/CMakeFiles/Ex3.dir/driver.c.obj"
+  "C:/Users/Almog/OneDrive - Technion/Simester 3/234122 - Introduction to Systems Programming/Homework/HW2/Formula1/main.c" "C:/Users/Almog/OneDrive - Technion/Simester 3/234122 - Introduction to Systems Programming/Homework/HW2/Formula1/cmake-build-debug/CMakeFiles/Ex3.dir/main.c.obj"
+  "C:/Users/Almog/OneDrive - Technion/Simester 3/234122 - Introduction to Systems Programming/Homework/HW2/Formula1/team.c" "C:/Users/Almog/OneDrive - Technion/Simester 3/234122 - Introduction to Systems Programming/Homework/HW2/Formula1/cmake-build-debug/CMakeFiles/Ex3.dir/team.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Ex3.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ex3.dir/main.c.obj"
+  "CMakeFiles/Ex3.dir/driver.c.obj"
+  "CMakeFiles/Ex3.dir/team.c.obj"
+  "Ex3.pdb"
+  "Ex3.exe"
+  "Ex3.exe.manifest"
+  "libEx3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Ex3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
