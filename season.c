29c1216b6fb5fd1853d98c3e74bea23f055fa0df
Almog Tzabari <almogtzabari@campus.,technion.ac.il>
2018-04-30 13:11:05 +0300

    Added struct season

--- season.c
+++ season.c
@@ -1,4 +1,14 @@
-//
-// Created by Ariel Kfir on 27-Apr-18.
-//
+#include <stdio.h>
+#include <malloc.h>
+#include <string.h>
+#include <assert.h>
+#include "driver.h"
 
+
+typedef struct season {
+    int year;
+    int number_of_teams;
+    Team* team_array;
+    int number_of_drivers;
+    Driver* driver_array
+} *Season;
