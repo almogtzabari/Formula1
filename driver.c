14c000f69b3d1beb15ade1bce55e93202c30c3ea
Ariel Kfir <Arielkfir@campus.technion.ac.il>
2018-05-05 16:56:11 +0200

    added comments to driver.c

--- driver.c
+++ driver.c
@@ -18,12 +18,16 @@ struct driver {
     Season season_of_driver;
 };
 
-
+/**
+ ***** Function: DriverCreate *****
+ * Creates a new driver.
+ * @param status - will hold success or fail of the function.
+ * @param driver_name - A string which contains the driver's name.
+ * @param driverId - Driver id.
+ * @return returns NULL in case of failure or pointer to the created driver in case of success.
+ */
 Driver DriverCreate(DriverStatus* status, char* driver_name, int driverId){
-    if(driver_name==NULL){
-        *status=DRIVER_MEMORY_ERROR;
-        return NULL;
-    }
+    assert(status!=NULL && driver_name!=NULL);
     char* name = malloc(strlen(driver_name)+1);
     if (name==NULL){
         *status=DRIVER_MEMORY_ERROR;
@@ -36,11 +40,11 @@ Driver DriverCreate(DriverStatus* status, char* driver_name, int driverId){
         free(name);
         return NULL;
     }
-//    if(driverId<0){
-//        *status=DRIVER_MEMORY_ERROR;
-//        free(name);
-//        return NULL;
-//    }
+    if(driverId<0){
+        *status=DRIVER_MEMORY_ERROR;
+        free(name);
+        return NULL;
+    }
     driver->id=driverId;
     driver->driver_name=name;
     driver->season_of_driver = NULL;
@@ -48,13 +52,19 @@ Driver DriverCreate(DriverStatus* status, char* driver_name, int driverId){
     *status=DRIVER_STATUS_OK;
     return driver;
 }
-
+/**
+ ***** Function : DriverSetTeam *****
+ * Sets given team to a given driver.
+ * @param driver A pointer to a driver.
+ * @param team A pointer to a team.
+ */
 void DriverSetTeam(Driver driver, Team team){
     assert(driver!=NULL && team!=NULL);
     driver->team=team;
 }
 /** Tested: Yes
  ***** Function: DriverGetName *****
+ * Gets a driver name.
  * @param driver - A pointer to a driver.
  * @return Name of the driver.
  */
@@ -65,6 +75,12 @@ const char* DriverGetName(Driver driver){
     return driver->driver_name;
 }
 
+/**
+ ***** Function: DriverGetTeam *****
+ * Gets the driver's team pointer.
+ * @param driver - A pointer to a driver.
+ * @return A pointer to a team if driver is not a NULL pointer.
+ */
 Team DriverGetTeam(Driver driver){
     if(driver==NULL){
         return NULL;
@@ -74,6 +90,7 @@ Team DriverGetTeam(Driver driver){
 
 /** Tested: Yes
  ***** Function: DriverGetId *****
+ * Gets the given driver's id.
  * @param driver - A pointer to a driver.
  * @return ID of the driver.
  */
@@ -84,17 +101,34 @@ int DriverGetId(Driver driver){
     return driver->id;
 }
 
+/**
+ ***** Function: DriverDestroy *****
+ * Destroys the allocated memory of all resources of a given driver.
+ * @param driver - A pointer to a driver.
+ */
 void DriverDestroy(Driver driver){
     free(driver->driver_name);
     free(driver);
 }
-
+/**
+ ***** Function: DriverSetSeason *****
+ * Sets a given season to a given driver.
+ * @param driver - A pointer to a driver.
+ * @param season - A pointer to a season.
+ */
 void DriverSetSeason(Driver driver, Season season){
     assert(driver!=NULL && season!=NULL);
     driver->season_of_driver = season;
     driver->points=0;
 }
 
+/**
+ ***** Function: DriverAddRaceResult *****
+ * Adds points to a driver by it's position.
+ * @param driver - A pointer to a driver.
+ * @param position - The driver's position in the last race.
+ * @return Success/Failure of the function.
+ */
 DriverStatus DriverAddRaceResult(Driver driver, int position){
     if(driver == NULL){
         return INVALID_DRIVER;
@@ -111,11 +145,13 @@ DriverStatus DriverAddRaceResult(Driver driver, int position){
 
 /** Tested: Yes
  ***** Function: DriverGetPoints *****
+ * Gets a driver's points.
  * @param driver - A pointer to a driver.
  * @param status - Will hold the success/fail of the function.
  * @return number of points of 'driver'.
  */
 int DriverGetPoints(Driver driver, DriverStatus* status){
+    assert(status!=NULL);
     if (driver==NULL){
         *status=INVALID_DRIVER;
         return 0;
